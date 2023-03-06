c8fdc6097c1192038b5f2ff71cb45c89c88da53d
Kola <ask_kol@yaoo.co.uk>
2023-03-06 13:32:14 +0000

    Add cors permissions in Basket.Api

--- src/Microservices/BasketService/Basket.Api/Program.cs
+++ src/Microservices/BasketService/Basket.Api/Program.cs
@@ -1,11 +1,12 @@
-using Basket.Api.CustomMiddleware;
-using Basket.Api.Repositories;
-
+const string ALllowAllCors = "AllowAllCors";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(opt => opt.AddPolicy(
+    ALllowAllCors, corsPolicyBuilder =>
+        corsPolicyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
